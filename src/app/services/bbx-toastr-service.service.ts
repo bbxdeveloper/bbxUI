df2a49fad3c89b3f764947d4733d4d54e97ff679
Jeremi <kdrisaac42@gmail.com>
2022-03-06 00:01:18 +0100

    handling multiple toastr

--- src/app/services/bbx-toastr-service.service.ts
+++ src/app/services/bbx-toastr-service.service.ts
@@ -9,22 +9,26 @@ export class BbxToastrService {
   private _toastrRef?: NbToastRef;
   private _toastrOpened: boolean = false;
 
-  get IsToastrOpened() { return this._toastrOpened; }
+  private toastrRefStack: NbToastRef[] = [];
+
+  get IsToastrOpened() { return this.toastrRefStack.length > 0; }
 
   constructor(private toastrService: NbToastrService) { }
 
   show(message: any, title?: any, userConfig?: Partial<NbToastrConfig>): NbToastRef {
     this._toastrRef = this.toastrService.show(message, title, userConfig);
+    this.toastrRefStack.push(this._toastrRef);
     this._toastrRef.onClose().subscribe({
-      next: () => { this._toastrOpened = false; }
+      next: () => { this._toastrOpened = false; this.toastrRefStack.pop(); }
     });
     this._toastrOpened = true;
     return this._toastrRef;
   }
 
   close(): void {
-    if (this._toastrOpened) {
-      this._toastrRef?.close();
+    if (this.IsToastrOpened) {
+      const tmp = this.toastrRefStack[this.toastrRefStack.length - 1] ?? undefined;
+      tmp?.close();
     }
   }
 }
