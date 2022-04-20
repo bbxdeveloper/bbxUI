4371b4377362d79105121831c6d18909f177f3aa
Jeremi <kdrisaac42@gmail.com>
2022-04-21 01:29:54 +0200

    Dev build mark with version number.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { NbSidebarService } from '@nebular/theme';
 import { ElectronService } from 'ngx-electron';
 import { environment } from 'src/environments/environment';
+import packageJson from '../../package.json';
 
 @Component({
   selector: 'app-root',
@@ -9,7 +10,9 @@ import { environment } from 'src/environments/environment';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  headerTitle = 'BBX';
+  headerTitle: string = 'BBX';
+  version: string = '';
+  devVersion: boolean = false;
 
   constructor(private es: ElectronService, private sidebarService: NbSidebarService) { }
 
@@ -17,8 +20,12 @@ export class AppComponent implements OnInit {
     setTimeout(() => {
       this.sidebarService.collapse();
     }, 200);
+    
     environment.electron = this.es.isElectronApp;
     environment.electron ?
       this.headerTitle += " (desktop)" : this.headerTitle += " (web)";
+    
+    this.devVersion = !environment.production;
+    this.version = packageJson.version;
   }
 }
