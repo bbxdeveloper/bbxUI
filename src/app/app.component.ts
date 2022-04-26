1d98d1b629659051ef987d42a424eae96f6ea960
Jeremi <kdrisaac42@gmail.com>
2022-04-26 21:04:27 +0200

    Show version as part of the browser tab title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NbSidebarService } from '@nebular/theme';
 import { ElectronService } from 'ngx-electron';
+import { Title } from '@angular/platform-browser';
 import { environment } from 'src/environments/environment';
 import packageJson from '../../package.json';
 
@@ -14,7 +15,7 @@ export class AppComponent implements OnInit {
   version: string = '';
   devVersion: boolean = false;
 
-  constructor(private es: ElectronService, private sidebarService: NbSidebarService) { }
+  constructor(private es: ElectronService, private sidebarService: NbSidebarService, private title: Title) { }
 
   ngOnInit() {
     setTimeout(() => {
@@ -22,10 +23,10 @@ export class AppComponent implements OnInit {
     }, 200);
     
     environment.electron = this.es.isElectronApp;
-    environment.electron ?
-      this.headerTitle += " (desktop)" : this.headerTitle += " (web)";
     
     this.devVersion = !environment.production;
     this.version = packageJson.version;
+
+    this.title.setTitle('BBX v' + this.version);
   }
 }
