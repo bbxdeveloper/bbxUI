a165bbf65fc06e99af2b170e1866798935ec920e
Tamás Kis <kis6724@gmail.com>
2023-03-18 19:12:10 +0100

    open the cont-invctrl component

---
+++ src/app/modules/inventory/cont-inv-ctrl/cont-inv-ctrl.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cont-inv-ctrl',
+  templateUrl: './cont-inv-ctrl.component.html',
+  styleUrls: ['./cont-inv-ctrl.component.scss']
+})
+export class ContInvCtrlComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
