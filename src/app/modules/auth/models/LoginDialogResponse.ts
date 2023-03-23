81b4408344788aa3068b0bf28ebfc98d01d74c19
Jeremi <kdrisaac42@gmail.com>
2023-03-23 21:04:48 +0100

    Selecting warehouse on login

--- src/app/modules/auth/models/LoginDialogResponse.ts
+++ src/app/modules/auth/models/LoginDialogResponse.ts
@@ -1,5 +1,8 @@
+import { WareHouse } from "../../warehouse/models/WareHouse";
+
 export interface LoginDialogResponse {
     answer: boolean;
     name: string;
     pswd: string;
+    wareHouse: WareHouse
 }
