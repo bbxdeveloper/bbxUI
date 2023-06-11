d8468b145456c4652c0669571245346ba52db53e
Jeremi <kdrisaac42@gmail.com>
2023-06-11 09:24:08 +0200

    customer invoice summary update

--- src/app/modules/invoice/customer-invoice-summary/customer-invoice-summary-filter-form/CustomerInvoiceSummaryFilterFormData.ts
+++ src/app/modules/invoice/customer-invoice-summary/customer-invoice-summary-filter-form/CustomerInvoiceSummaryFilterFormData.ts
@@ -1,7 +1,21 @@
-export interface CustomerInvoiceSummaryFilterFormData {
-    Incoming: boolean
-    InvoiceDeliveryDateFrom: string
-    InvoiceDeliveryDateTo?: string
-    CustomerID?: number
-    WarehouseCode?: string
+import { HelperFunctions } from "src/assets/util/HelperFunctions"
+
+export class CustomerInvoiceSummaryFilterFormData {
+    Incoming: boolean = false
+    InvoiceDeliveryDateFrom: string = ''
+    InvoiceDeliveryDateTo?: string = ''
+    CustomerID?: number = -1
+    CustomerSearch?: string = ''
+    CustomerName?: string = ''
+    CustomerAddress?: string = ''
+    CustomerTaxNumber?: string = ''
+    WarehouseCode?: string = ''
+
+    public static create(): CustomerInvoiceSummaryFilterFormData {
+        return {
+            InvoiceDeliveryDateFrom: HelperFunctions.GetDateString(0, 0, 0)
+        } as CustomerInvoiceSummaryFilterFormData
+    }
+
+    protected constructor() { }
 }
