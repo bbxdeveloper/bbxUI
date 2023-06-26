f15ba4dd7785bacc3d74490ce87deab49d064a6e
Jeremi <kdrisaac42@gmail.com>
2023-06-26 23:35:37 +0200

    combobox fix, horizontal-split-layout update, customer invoice summary update

--- src/app/modules/shared/layouts/hozirontal-split-layout/hozirontal-split-layout.component.ts
+++ src/app/modules/shared/layouts/hozirontal-split-layout/hozirontal-split-layout.component.ts
@@ -7,11 +7,30 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class HozirontalSplitLayoutComponent implements OnInit {
   @Input() proportion: string = '30'
+  @Input() usePixelForProportions: boolean = false
   @Input() hideDivider: boolean = false
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  get customStyles(): string {
+    let css = ''
+
+    // if (this.usePixelForProportions) {
+    //   css += `layout-proportionPixelMax-of-${this.proportion} `
+    // } else {
+    //   css += `layout-proportion-of-${this.proportion} `
+    // }
+
+    if (this.usePixelForProportions) {
+      css += `grid-template-rows: calc(0% + ${this.proportion}px) 1% calc(99% - ${this.proportion}px);`
+    } else {
+      css += `grid-template-rows: calc(0% + ${this.proportion}%) 1% calc(99% - ${this.proportion}%);`
+    }
+
+    return css;
   }
 
 }
