59dbb363f73cd9bbffc0d6599fa0ec9f58aaae00
Jeremi <kdrisaac42@gmail.com>
2023-07-24 21:30:54 +0200

    user login when saving invoice part 4 - env fix

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -33,7 +33,7 @@ export const environment = {
 
   InvSaveDlgLogs: true,
 
-  theme: 'cosmic-light',
+  theme: 'cosmic-custom',
 
   userManualsLink: 'https://drive.google.com/drive/folders/1wzrCpPJK6gEcTVbINSETUDt8J7_X8IIg?usp=sharing'
 };
