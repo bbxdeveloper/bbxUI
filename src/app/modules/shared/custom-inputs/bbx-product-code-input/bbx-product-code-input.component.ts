62ec23b39846ece398fb57e0e24ad3f0a8bf9f3b
Tamás Kis <kis6724@gmail.com>
2023-09-22 22:22:57 +0200

    Revert "handling code modification"
    This reverts commit a79109c074a8508fbfec149f7c560b0727d3eec3.

--- src/app/modules/shared/custom-inputs/bbx-product-code-input/bbx-product-code-input.component.ts
+++ src/app/modules/shared/custom-inputs/bbx-product-code-input/bbx-product-code-input.component.ts
@@ -1,8 +1,9 @@
-import { Component, EventEmitter, Input, OnInit, Output, forwardRef } from '@angular/core';
+import { Component, EventEmitter, HostListener, Input, OnInit, Output, ViewEncapsulation, forwardRef } from '@angular/core';
 import { AbstractControl, ControlValueAccessor, NG_VALIDATORS, NG_VALUE_ACCESSOR, ValidationErrors, Validator } from '@angular/forms';
 import { noop } from 'rxjs';
 import { LoggerService } from 'src/app/services/logger.service';
 import { Constants } from 'src/assets/util/Constants';
+import { ProductCodeManagerServiceService } from 'src/app/services/product-code-manager-service.service';
 
 @Component({
   selector: 'app-bbx-product-code-input',
@@ -35,7 +36,7 @@ export class BbxProductCodeInputComponent implements OnInit, ControlValueAccesso
   @Input() disabled: boolean = false
   @Input() required: boolean = false
   @Input() readonly: boolean = false
-
+  
   touched: boolean = false
 
   // Output
@@ -52,10 +53,10 @@ export class BbxProductCodeInputComponent implements OnInit, ControlValueAccesso
   mask = Constants.ProductCodeMask;
 
   // Value, callbacks...
-
+  
   private onTouchedCallback: () => void = noop
   private onChangeCallback: (newValue: any) => void = noop
-
+  
   private innerValue: any = ''
 
   get value(): any {
@@ -63,18 +64,8 @@ export class BbxProductCodeInputComponent implements OnInit, ControlValueAccesso
   }
   set value(v: any) {
     this.log(`[BbxProductCodeInputComponent] set value, current value: '${this.value}', new value: '${v}'`)
-    if (!v) {
-      return
-    }
-
     this.markAsTouched()
     if (v !== this.innerValue) {
-      if (this.innerValue.length > 0 && v.length - this.innerValue.length === 2) {
-        if (v.length !== 4) {
-          v = v.slice(0, -1)
-        }
-      }
-
       this.innerValue = v
       this.onChangeCallback(v)
     }
@@ -147,7 +138,7 @@ export class BbxProductCodeInputComponent implements OnInit, ControlValueAccesso
     return null
   }
   registerOnValidatorChange?(fn: () => void): void {
-
+    
   }
 
   // Product code handling, selecting products...
