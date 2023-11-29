aaaa34db3c4b15eaccc2cb206daeaace6d841444
Tamás Kis <kis6724@gmail.com>
2023-11-29 18:46:07 +0100

    remove code

--- src/app/modules/invoice/save-dialog/save-dialog.component.ts
+++ src/app/modules/invoice/save-dialog/save-dialog.component.ts
@@ -47,7 +47,6 @@ export class SaveDialogComponent extends BaseNavigatableComponentComponent imple
   @Input() forceDisableOutgoingDelivery: boolean = false
   @Input() negativeDiscount: boolean = false
   @Input() defaultDiscountPercent?: number
-  @Input() rountToFillér = false
 
   @Input() checkCustomerLimit: boolean = false
   @Input() customer?: Customer
@@ -249,9 +248,7 @@ export class SaveDialogComponent extends BaseNavigatableComponentComponent imple
       discountedInvoiceNetAmount = this.invoiceStats.SummaryInvoiceDiscountedNetAmountSum;
     }
 
-    discountedInvoiceNetAmount = this.rountToFillér
-      ? HelperFunctions.Round2(discountedInvoiceNetAmount, 1)
-      : HelperFunctions.Round2(discountedInvoiceNetAmount, 1);
+    discountedInvoiceNetAmount = HelperFunctions.Round2(discountedInvoiceNetAmount, 1);
 
     if (!this.isAggregate){
       let invoiceDiscountValue = this.data.invoiceNetAmount - discountedInvoiceNetAmount;
