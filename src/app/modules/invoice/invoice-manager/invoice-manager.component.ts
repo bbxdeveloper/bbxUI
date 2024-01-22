2c3adb8c528456014c317bfc492fe506c74ad70d
Tamás Kis <kis6724@gmail.com>
2024-01-22 21:58:29 +0100

    hightlight fix

--- src/app/modules/invoice/invoice-manager/invoice-manager.component.ts
+++ src/app/modules/invoice/invoice-manager/invoice-manager.component.ts
@@ -485,21 +485,6 @@ export class InvoiceManagerComponent extends BaseInvoiceManagerComponent impleme
         }),
         tap(value => this.outGoingInvoiceData.currencyCode = value),
         tap(value => this.exchangeRateVisible.next(value !== CurrencyCodes.HUF)),
-        tap(value => setTimeout(() => {
-          this.outInvFormNav.GenerateAndSetNavMatrices(false)
-
-          if (value === CurrencyCodes.HUF) {
-            this.kbS.ClickCurrentElement()
-          } else {
-            const element = document.getElementsByName("exchange-rate")[0]
-
-            // @workaround - the input gets a `text-align: right` somehow
-            // reset here
-            element.style.textAlign = 'unset';
-
-            this.kbS.ClickElement(element.id)
-          }
-        }, 100)),
         switchMap((value: string) => value !== CurrencyCodes.HUF ? of(value) : EMPTY),
         tap((value: string) => this.exchangeRateQuery.next(value as CurrencyCodes)),
       )
@@ -709,9 +694,30 @@ export class InvoiceManagerComponent extends BaseInvoiceManagerComponent impleme
         } as GetExchangeRateParamsModel)),
         switchMap(value => this.systemService.GetExchangeRate(value)),
         tap(() => this.isLoading = false),
+        tap(value => setTimeout(() => {
+          this.outInvForm.controls['exchangeRate'].setValue(value);
+
+          this.outInvFormNav.GenerateAndSetNavMatrices(false)
+
+          if (this.outGoingInvoiceData.currencyCode === CurrencyCodes.HUF) {
+            this.kbS.ClickCurrentElement()
+            return
+          }
+
+          const element = document.querySelector('[name="exchange-rate"] input') as HTMLInputElement
+
+          // @workaround - the input gets a `text-align: right` somehow
+          // reset here
+          element.style.textAlign = 'unset'
+
+          setTimeout(() => {
+            this.kbS.SelectElement(element.id)
+            this.kbS.ClickCurrentElement()
+          }, 150)
+        }, 100)),
       )
       .subscribe({
-        next: (response) => this.outInvForm.controls['exchangeRate'].setValue(response),
+        next: () => {},
         error: error => {
           this.cs.HandleError(error)
           this.isLoading = false
