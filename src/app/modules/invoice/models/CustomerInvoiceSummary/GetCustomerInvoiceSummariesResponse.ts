75ad8bdc48a256d6225e5d1aabfd3b1d71603da3
Tamás Kis <kis6724@gmail.com>
2024-03-19 18:48:41 +0100

    Add summaryNet, summaryVat and summaryGross

--- src/app/modules/invoice/models/CustomerInvoiceSummary/GetCustomerInvoiceSummariesResponse.ts
+++ src/app/modules/invoice/models/CustomerInvoiceSummary/GetCustomerInvoiceSummariesResponse.ts
@@ -1,4 +1,8 @@
 import { IResponse } from "src/assets/model/IResponse";
 import { CustomerInvoiceSummary } from "./CustomerInvoiceSummary";
 
-export interface GetCustomerInvoiceSummariesResponse extends IResponse<CustomerInvoiceSummary> { }
+export interface GetCustomerInvoiceSummariesResponse extends IResponse<CustomerInvoiceSummary> {
+    summaryNet: number
+    summaryVat: number
+    summaryGross: number
+}
